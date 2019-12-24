ebfb2fee06175441c9bfcb1f4979196b1446b1f8
chenyuantao <cyt528300@gmail.com>
2019-12-24 10:37:51 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,57 +1,33 @@
+/* eslint-disable */
 import React from 'react';
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useWindowSize } from 'react-use';
+import Confetti from 'react-confetti';
 
-function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
+export default () => {
+  const { width, height } = useWindowSize();
+  console.log(width, height);
+
   return (
-    <main>
-      <h1>Create React App + Go API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://zeit.co/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          ZEIT Now
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/zeit/now-examples/tree/master/create-react-app-functions"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
-    </main>
+    <div style={{ width: '100vh', height: '100vh' }}>
+      <div
+        style={{
+          position: 'absolute',
+          left: 0,
+          top: 0,
+          right: 0,
+          bottom: 0,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <img src="/gr_logo.png" style={{ width: 100 }} />
+        <div style={{ color: 'red', marginTop: 10 }}>
+          🎄🧦🎁各位肉肉们圣诞快乐🎄🧦🎁
+        </div>
+      </div>
+      <Confetti />
+    </div>
   );
-}
-
-export default App;
+};
